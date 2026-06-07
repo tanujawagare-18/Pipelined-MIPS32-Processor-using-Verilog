63c23efb5a8a3920548cd06c0ff933ab6ce4b49b
tanujawagare-18 <tanaviwagare@gmail.com>
2026-06-07 16:06:20 +0530

    Update add_mips32.v

--- add_mips32.v
+++ add_mips32.v
@@ -23,7 +23,7 @@ initial
       mips.Mem[0] = 32'h2801000a;  //ADDI R1,R0,10
       mips.Mem[1] = 32'h28020014;  //ADDI R2,R0,20
       mips.Mem[2] = 32'h28030019;  //ADDI R3,R0,25
-      mips.Mem[3] = 32'h0ce77800;  //OR R7,R7,R7
+      mips.Mem[3] = 32'h0ce77800;  //OR R7,R7,R7  (dummy value)
       mips.Mem[4] = 32'h0ce77800;  //OR R7,R7,R7
       mips.Mem[5] = 32'h00222000;  //ADD R4,R1,R2
       mips.Mem[6] = 32'h0ce77800;  //OR R7,R7,R7
